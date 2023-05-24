aa9f58f63e69265100b584980b6f1935309e00d9
Eykenis <ekennis@outlook.com>
2023-05-25 00:42:13 +0800

    Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -248,6 +248,30 @@ uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm)
   return newsz;
 }
 
+void recur_vmprint(pagetable_t pagetable, int level){
+  for (int i = 0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      uint64 child = PTE2PA(pte);
+      for (int j = 0; j < level; ++j)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      recur_vmprint((pagetable_t)child, level + 1);
+    }
+    else if (pte & PTE_V){
+      for (int j = 0; j < level; ++j)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  recur_vmprint(pagetable, 0);
+}
+
 // Deallocate user pages to bring the process size from oldsz to
 // newsz.  oldsz and newsz need not be page-aligned, nor does newsz
 // need to be less than oldsz.  oldsz can be larger than the actual
